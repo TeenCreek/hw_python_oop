57b286962e81d866829b9f559f5f16aae0311f67
Артём Хрущёв <art6668@yandex.ru>
2022-10-15 09:20:30 +0700

    Refactoring

--- homework.py
+++ homework.py
@@ -162,5 +162,5 @@ if __name__ == "__main__":
     ]
 
     for workout_type, data in packages:
-        training: Type[Training] = read_package(workout_type, data)
+        training: Training = read_package(workout_type, data)
         main(training)
